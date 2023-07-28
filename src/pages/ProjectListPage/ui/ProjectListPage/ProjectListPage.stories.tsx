1b6f6f36d89e0b908d5f9f3542b28cdb134ad229
Muratov Ivan <ivanmuratov121096@gmail.com>
2023-07-28 19:18:59 +0700

    add story-cases ProjectBoardPage, ProjectTaskListPage

--- src/pages/ProjectListPage/ui/ProjectListPage/ProjectListPage.stories.tsx
+++ src/pages/ProjectListPage/ui/ProjectListPage/ProjectListPage.stories.tsx
@@ -2,7 +2,17 @@ import { ComponentStory, ComponentMeta } from '@storybook/react';
 
 import ProjectListPage from './ProjectListPage';
 
+import { ProjectSchema } from '@/entities/Project';
 import StoreDecorator from '@/shared/config/storybook/StoreDecorator/StoreDecorator';
+
+const projectList: ProjectSchema[] = new Array(10).fill('*').map((value, index) => ({
+  id: `${index}`,
+  name: 'project',
+  prefix: 'prefix',
+  description: 'description',
+  manager: 'manager',
+  countTasks: 10,
+}));
 
 export default {
   title: 'pages/ProjectListPage/ProjectListPage',
@@ -11,18 +21,18 @@ export default {
     backgroundColor: { control: 'color' },
   },
   decorators: [StoreDecorator],
-  parameters: {
-    mockData: [
-      {
-        url: `${__API__}/projects`,
-        method: 'GET',
-        status: 200,
-        response: [],
-      },
-    ],
-  },
 } as ComponentMeta<typeof ProjectListPage>;
 
 const Template: ComponentStory<typeof ProjectListPage> = args => <ProjectListPage {...args} />;
 
 export const Normal = Template.bind({});
+Normal.parameters = {
+  mockData: [
+    {
+      url: `${__API__}/projects`,
+      method: 'GET',
+      status: 200,
+      response: projectList,
+    },
+  ],
+};
--- src/pages/ProjectTaskListPage/ui/ProjectTaskListPage/ProjectTaskListPage.stories.tsx
+++ src/pages/ProjectTaskListPage/ui/ProjectTaskListPage/ProjectTaskListPage.stories.tsx
@@ -2,7 +2,21 @@ import { ComponentStory, ComponentMeta } from '@storybook/react';
 
 import ProjectTaskListPage from './ProjectTaskListPage';
 
+import { TaskSchema } from '@/entities/Task';
 import StoreDecorator from '@/shared/config/storybook/StoreDecorator/StoreDecorator';
+
+const taskList: TaskSchema[] = new Array(20).fill('*').map((val, index) => ({
+  id: `${index}`,
+  name: `task - ${index}`,
+  description: 'description',
+  category: 'category',
+  author: 'author',
+  executor: 'executor',
+  createdDate: new Date().toLocaleDateString(),
+  status: 'to do',
+  type: 'task',
+  priority: '0',
+}));
 
 export default {
   title: 'pages/ProjectTaskListPage/ProjectTaskListPage',
@@ -11,6 +25,16 @@ export default {
     backgroundColor: { control: 'color' },
   },
   decorators: [StoreDecorator],
+  parameters: {
+    mockData: [
+      {
+        url: `${__API__}/tasks?projectId_like=1&q=&_page=1&_limit=10`,
+        method: 'GET',
+        status: 200,
+        response: taskList,
+      },
+    ],
+  },
 } as ComponentMeta<typeof ProjectTaskListPage>;
 
 const Template: ComponentStory<typeof ProjectTaskListPage> = args => (
